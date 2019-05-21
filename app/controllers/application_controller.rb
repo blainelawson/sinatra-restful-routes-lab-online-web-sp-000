336012fa0157f4ab5fd8dbe1388a012f3034e1e9
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-21 18:29:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
+<<<<<<< HEAD
   post '/recipes' do
     # binding.pry
     recipe = Recipe.new(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
@@ -26,24 +27,43 @@ class ApplicationController < Sinatra::Base
   #   redirect "/recipes/#{@recipe.id}"
   # end
 
+=======
+  post '/recipes/new' do
+    # binding.pry
+    @recipe = Recipe.new(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+binding.pry
+    @recipe.save
+
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+>>>>>>> 949d547e43215ab62ccf10c41a1885edc4e233aa
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
 
   get '/recipes/:id' do
+<<<<<<< HEAD
     @recipe = Recipe.find_by_id(params[:id])
+=======
+    @recipe = Recipe.find_by(params[:id])
+>>>>>>> 949d547e43215ab62ccf10c41a1885edc4e233aa
 
     erb :show
   end
 
   get '/recipes/:id/edit' do
+<<<<<<< HEAD
     # binding.pry
+=======
+>>>>>>> 949d547e43215ab62ccf10c41a1885edc4e233aa
       @recipe = Recipe.find_by_id(params[:id])
 
       erb :edit
   end
 
+<<<<<<< HEAD
   patch '/recipes/:id' do
     # binding.pry
     recipe = Recipe.find_by_id(params[:id])
@@ -58,6 +78,10 @@ class ApplicationController < Sinatra::Base
   delete '/recipes/:id' do
     # binding.pry
     recipe = Recipe.find_by_id(params[:id])
+=======
+  delete '/recipes/:id' do
+    recipe = Article.find_by_id(params[:id])
+>>>>>>> 949d547e43215ab62ccf10c41a1885edc4e233aa
     recipe.delete
     redirect '/recipes'
   end
